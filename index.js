3b3dc250cb6e9d017a53388a7cab90d5b18a478e
TAAMEFL2 <florence.meier@swisscom.com>
2019-08-24 13:26:08 +0200

    add dummyProducts

--- index.js
+++ index.js
@@ -29,31 +29,66 @@ app.get('/abos', (req, res) => {
 
 app.post('/abos', (req, res) => {
     console.log('Abo page')
-    //res.redirect('/abos')
     const dummyData = [
         {
-          id: 1,
+          key: 1,
           title: "Rüebli Abo",
           hersteller: "Max Muster",
           preis: "25.00 Taler"
         },
         {
-          id: 2,
+          key: 2,
           title: "Sunneschiin Früchte",
           hersteller: "Hans Muster",
           preis: "55.00 Taler"
         },
         {
-          id: 3,
+          key: 3,
           title: "Blumen Mittel",
-          hersteller: "Grigitte Muster",
+          hersteller: "Brigitte Muster",
           preis: "14.00 Taler"
         }
       ]
     
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify(dummyData))
-    console.log(res);
+    return res;
+});
+
+app.post('/produkte', (req, res) => {
+    console.log('Produkte Abfrage')
+    const dummyData = [
+        {
+          key: 1,
+          name: "Karotte",
+          producer: "Max Muster",
+          price: "2.00 Taler",
+          co2Sparen: "15 g",
+          imageURL: "https://www.gesundheit.de/sites/default/files/styles/crop_content/public/2016-03/karotte.jpg?itok=MrGiGvSb",
+          onSale: false
+        },
+        {
+        key: 2,
+        name: "Kartoffel",
+        producer: "Max Muster",
+        price: "3.00 Taler",
+        co2Sparen: "22 g",
+        imageURL: "",
+        onSale: true
+        },
+        {
+        key: 3,
+        name: "Bio-Ei",
+        producer: "Herbert Meier",
+        price: "1 Taler",
+        co2Sparen: "9 g",
+        imageURL: "",
+        onSale: false
+        }
+      ]
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(dummyData))
     return res;
 });
 
