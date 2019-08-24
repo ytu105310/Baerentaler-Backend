9b829bb29b60ec6b6a4c391a6242754f7f62c91b
TAAMEFL2 <florence.meier@swisscom.com>
2019-08-24 11:44:11 +0200

    Add mock data for abo

--- index.js
+++ index.js
@@ -29,7 +29,32 @@ app.get('/abos', (req, res) => {
 
 app.post('/abos', (req, res) => {
     console.log('Abo page')
-    res.redirect('/abos')
+    //res.redirect('/abos')
+    const dummyData = [
+        {
+          id: 1,
+          title: "Rüebli Abo",
+          hersteller: "Max Muster",
+          preis: "25.00 Taler"
+        },
+        {
+          id: 2,
+          title: "Sunneschiin Früchte",
+          hersteller: "Hans Muster",
+          preis: "55.00 Taler"
+        },
+        {
+          id: 3,
+          title: "Blumen Mittel",
+          hersteller: "Grigitte Muster",
+          preis: "14.00 Taler"
+        }
+      ]
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(dummyData))
+    console.log(res);
+    return res;
 });
 
 var server = app.listen(port, function () {
